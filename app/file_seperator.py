ff5f26f925b9b5f59aad8e80cdd5863857223be5
limon <t.limon2342@gmail.com>
2021-04-07 11:42:09 +0600

    project done for deployment

--- app/file_seperator.py
+++ app/file_seperator.py
@@ -1,6 +1,8 @@
+# necessary module
+
 import os,shutil
 
-# file extensions --> you can write every single extensions inside the tuple
+# file extensions --> you can write every single extensions inside the list or tuple
 
 folders = {
     'audios' : ['.mp3','.mp4','.wav','.flac'],
@@ -10,18 +12,14 @@ folders = {
 
 }
 
-# for folder_name in folders:
-#     print(folder_name,folders[folder_name])
-    
-
-# directory = input('Enter your locations: ')
-
+# rename your folders
 def rename_folder():
     for folder in os.listdir(directory):
         if os.path.isdir(os.path.join(directory,folder))==True:
             os.rename(os.path.join(directory,folder),os.path.join(directory,folder.lower()))
 
 
+# moving your files into the desire folders
 def create_move(extension,file_name):
     fname = False
     for folder_name in folders:
@@ -33,6 +31,7 @@ def create_move(extension,file_name):
             shutil.move(os.path.join(directory,file_name),os.path.join(directory,folder_name))
             fname=True
             break
+    # moving unknown files into separate folder
     if fname !=True:
         if other_name not in os.listdir(directory):
             os.mkdir(os.path.join(directory,other_name))
@@ -40,6 +39,7 @@ def create_move(extension,file_name):
         shutil.move(os.path.join(directory,file_name),os.path.join(directory,other_name))
 
 
+# all important files and folders
 
 directory = "F:\\myfile"
 other_name = input('Enter other name: ')
@@ -48,9 +48,9 @@ all_files = os.listdir(directory)
 length = len(all_files)
 count=1
 
-# print(all_files)
 
 
+# functions
 for i in all_files:
     if os.path.isfile(os.path.join(directory,i))== True:
         create_move(i.split('.')[-1],i)
