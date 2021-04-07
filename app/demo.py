855e5ad47bd9058b3c2a9a8e2b4a7ae24d875094
limon <t.limon2342@gmail.com>
2021-04-07 19:02:33 +0600

    modifiy

--- app/demo.py
+++ app/demo.py
@@ -7,35 +7,4 @@ folders = {
     'documents' : ['.pdf','.doc','.txt','.xlsx','.rar','.zip','.xls','.pptx'],
 }
 
-def create_move(extension,file_name):
-    found = False
-    for folder_name in folders:
-        if '.'+extension in folders[folder_name]:
-            if folder_name not in os.listdir(directory):
-                os.mkdir(os.path.join(directory,folder_name))
-            shutil.move(os.path.join(directory,file_name),os.path.join(directory,folder_name))
-            found = True
-            break
-    if found !=True:
-        if other_name not in os.listdir(directory):
-            os.mkdir(os.path.join(directory,other_name))
-        
-        shutil.move(os.path.join(directory,file_name),os.path.join(directory,other_name))
-
-
-
-
-directory = "F:\\myfile"
-all_files = os.listdir(directory)
-other_name = input('Enter the others directory: ')
-
-for i in all_files:
-    if os.path.isfile(os.path.join(directory,i))==True:
-        create_move(i.split('.')[-1],i)
-
-
-
-
-
-
 
